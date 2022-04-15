0be22c32ee2c3fc6002a8aeb2ab9e8abb9a2853c
HarshaVardhan <athaluri.harsha@gmail.com>
2022-04-15 09:11:51 +0530

    Inputs

---
+++ Inputs/src/app/home/home.component.ts
@@ -0,0 +1,15 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent {
+
+  @Input()
+  temp:any;
+
+
+  
+}
